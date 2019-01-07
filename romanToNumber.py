2bbf7fb9ada9e563c2a90f9b2e47c94e942f5cec
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-07 09:36:03 +0000

    Create romanToNumber.py

---
+++ romanToNumber.py
@@ -0,0 +1,43 @@
+s = input()
+s = s.upper()
+i=1
+if(s=='I'):
+  print(i)
+elif(s=='II'):
+  print(2)
+elif(s=='III'):
+  print(3)
+elif(s=='IV'):
+  print(4)
+elif(s=='V'):
+  print(5)
+elif(s=='VI'):
+  print(6)
+elif(s=='VII'):
+  print(7)
+elif(s=='VIII'):
+  print(8)
+elif(s=='IX'):
+  print(9)
+elif(s=='X'):
+  print(10)
+elif(s=='XI'):
+  print(11)
+elif(s=='XII'):
+  print(12)
+elif(s=='XIII'):
+  print(13)
+elif(s=='XIV'):
+  print(14)
+elif(s=='XV'):
+  print(15)
+elif(s=='XVI'):
+  print(16)
+elif(s=='XVII'):
+  print(17)
+elif(s=='XVIII'):
+  print(18)
+elif(s=='XIX'):
+  print(19)
+else:
+  print(20)
