5f699e9622b643847325c1cae0627baf679681c2
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-10 19:04:45 +0530

    Update evenOddindex.py

--- evenOddindex.py
+++ evenOddindex.py
@@ -1,15 +1,15 @@
 n = int(input())
 li = list(map(int,input().split()))
-res=[]
+result=[]
 x=0
 for i in range(len(li)):
     if(li[i]%2!=0 and x%2==0):
-        res.append(li[i])
+        result.append(li[i])
     elif(li[i]%2==0 and x%2!=0):
-        res.append(li[i])
+        result.append(li[i])
     x+=1
-for i in range (len(res)):
-    if(i==len(res)-1):
-        print(res[i])
+for i in range (len(result)):
+    if(i==len(result)-1):
+        print(result[i])
     else:
-        print(res[i],end=" ")
+        print(result[i],end=" ")
