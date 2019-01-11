16ec501026d18e136e79691876f44a320b0b27f8
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-11 18:37:35 +0530

    Update splitCandies.py

--- splitCandies.py
+++ splitCandies.py
@@ -1,23 +1,18 @@
 n1 = int(input())
 li = list(map(int,input().split()))
-temp=[]
 re=0
 flag=0
 for j in range(n1):
-    for i in range(len(li)):
-        if(li[i]>=1):
-            re+=1
-        if(li[i]>0):
-            li[i]-=1
-        ze = li.count(0)
-    if(ze==len(li)-1):
-        flag=1
-        break
+  for i in range(len(li)):
+    if(li[i]>=1):
+      re+=1
+    if(li[i]>0):
+      li[i]-=1
+    ze = li.count(0)
+  if(ze==len(li)-1):
+    flag=1
+    break
 if(flag==1):
-    temp.append(re+1)
-    for i in temp:
-        print(i)
+  print(re+1)
 else:
-    temp.append(re)
-    for i in temp:
-        print(i)
+  print(re)
