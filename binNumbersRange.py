d2229538d469759fc92859a1a600ce6b43e07ed7
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-14 14:39:51 +0530

    Update binNumbersRange.py

--- binNumbersRange.py
+++ binNumbersRange.py
@@ -1,6 +1,14 @@
 def convert_int(number,decimals):
     return str(number).zfill(decimals)
 n=int(input())
+li=[]
+x=0
 for i in range(2**n):
     temp = convert_int(int(bin(i)[2:]),n)
-    print(temp)
+    li.append(temp)
+print(li)
+for j in range(len(li)):
+    for i in range(len(li)):
+        if(li[i].count('1')==x):
+            print(li[i])
+    x+=1
