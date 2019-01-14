83374a90a7d93c7f060b8cff16f8f553e7c84c81
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-14 13:31:50 +0530

    Update binNumbersRange.py

--- binNumbersRange.py
+++ binNumbersRange.py
@@ -2,4 +2,5 @@ def convert_int(number,decimals):
     return str(number).zfill(decimals)
 n=int(input())
 for i in range(2**n):
-    print(convert_int(int(bin(i)[2:]),n))
+    temp = convert_int(int(bin(i)[2:]),n)
+    print(temp)
