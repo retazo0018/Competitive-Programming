d195c810601b9028f231a0668e60c8afb875b967
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-26 20:32:43 +0530

    Update listRotate.py

--- listRotate.py
+++ listRotate.py
@@ -3,5 +3,6 @@ n,k = map(int,input().split())
 li = deque(list(map(int,input().split())))
 for i in range(k):
     li.rotate(1)
-for i in li:
-    print(i,end=" ")
+for i in range(len(li)-1):
+    print(li[i],end=" ")
+print(li[len(li)-1])
