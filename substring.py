0efe2ca37f435bab29fc9f174f11f73db7d1bfbd
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-27 16:34:30 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,13 +1,16 @@
 s = input()
-flag=0
-le=0
 c=1
-for i in range(len(s)-1):
-    if(s[i]==s[i+1]):
+tot=0
+for i in range(1,len(s)):
+    if(s[i]==s[i-1]):
         c+=1
+        if(i == len(s)-1):
+            if(c>tot):
+                tot = c
+                ch = s[i-1]
     else:
-        if(c>le):
-            char = s[i]
-            le = c
+        if(c>tot):
+            tot = c
+            ch = s[i-1]
         c=1
-print(char,le)
+print(ch,tot)
