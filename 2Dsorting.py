f13e8fe320af36dcafa3a6b72f695149b0b2c45c
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-31 15:27:57 +0530

    Update 2Dsorting.py

--- 2Dsorting.py
+++ 2Dsorting.py
@@ -3,4 +3,8 @@ li=[]
 for i in range(r):
     temp = list(map(int,input().split()))
     li.append(sorted(temp))
-print(sorted(li))
+li.sort()
+for i in range(r):
+  for j in range(c):
+    print(li[i][j],end=" ")
+  print()
