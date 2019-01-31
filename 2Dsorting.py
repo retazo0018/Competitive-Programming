a6a4f2a400aa4a1f94e8b16a7bbf4ca9fa30339a
Ashwin Murali <ashwin.cse18@gmail.com>
2019-01-31 16:00:11 +0530

    Update 2Dsorting.py

--- 2Dsorting.py
+++ 2Dsorting.py
@@ -1,10 +1,17 @@
 r,c = map(int,input().split())
 li=[]
+t=[]
+res=[]
 for i in range(r):
     temp = list(map(int,input().split()))
     li.append(sorted(temp))
-li.sort()
+for i in range(c):
+  for j in range(r):
+    t.append(li[j][i])
+  res.append(sorted(t))
+  t=[]
 for i in range(r):
   for j in range(c):
-    print(li[i][j],end=" ")
+    print(res[j][i],end=" ")
   print()
+  
