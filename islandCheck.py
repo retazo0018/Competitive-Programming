09523dd488de563592729f100d2d207049b48136
Ashwin Murali <ashwin.cse18@gmail.com>
2019-02-04 16:23:36 +0530

    Update islandCheck.py

--- islandCheck.py
+++ islandCheck.py
@@ -4,7 +4,6 @@ print(li)
 c=0
 for i in range(n):
   li.insert(-1,[0]+[int(x) for x in input().split()]+[0])
-print(li)
 for i in range(1,n+1):
   for j in range(1,n+1):
     if(li[i][j]==1 and [[[li[i-1][k],li[i][k],li[i+1][k]]for k in range(j-1,j+2)]==[0,0,0],[0,1,0],[0,0,0]]):
