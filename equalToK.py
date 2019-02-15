18acf24a345e665cd7068d02c8a2118f530b2450
Ashwin Murali <ashwin.cse18@gmail.com>
2019-02-15 15:23:15 +0530

    Update equalToK.py

--- equalToK.py
+++ equalToK.py
@@ -4,9 +4,11 @@ flag=0
 for i in range(len(li)-1):
   for j in range(i+1,len(li)):
     if(li[i]+li[j]==k):
-      print("YES")
       flag=1
+      break
   if(flag==1):
     break
-if(flag==0):
+if(flag==1):
+  print("YES")
+else:
   print("NO")
